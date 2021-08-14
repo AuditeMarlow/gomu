be739f58acefb714cdcb1d133792137aa0adb11a
Niek den Breeje <n.j.den.breeje@gmail.com>
2021-08-14 14:54:16 +0200

    feat: Add stream command

--- cmd/cli/cli.go
+++ cmd/cli/cli.go
@@ -4,6 +4,7 @@ import (
 	"github.com/auditemarlow/gomu/cmd"
 	"github.com/auditemarlow/gomu/cmd/cli/call"
 	"github.com/auditemarlow/gomu/cmd/cli/new"
+	"github.com/auditemarlow/gomu/cmd/cli/stream"
 	"github.com/urfave/cli/v2"
 )
 
@@ -18,6 +19,11 @@ func init() {
 			Name:   "new",
 			Usage:  "gomu new greeter",
 			Action: new.Run,
+		},
+		&cli.Command{
+			Name:   "stream",
+			Usage:  "gomu stream greeter Say.HelloStream '{\"name\": \"John\"}'",
+			Action: stream.Run,
 		},
 	)
 }
