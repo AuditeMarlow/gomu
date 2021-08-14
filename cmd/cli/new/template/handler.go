98ebcba69b793d6636cd5759562cf4e4d0a816d3
Niek den Breeje <n.j.den.breeje@gmail.com>
2021-08-14 15:00:54 +0200

    fix: Sleep between server stream responses

--- cmd/cli/new/template/handler.go
+++ cmd/cli/new/template/handler.go
@@ -4,6 +4,7 @@ var Handler = `package handler
 
 import (
 	"context"
+	"time"
 
 	log "github.com/asim/go-micro/v3/logger"
 
@@ -21,11 +22,13 @@ func (e *{{title .Alias}}) Call(ctx context.Context, req *pb.Request, rsp *pb.Re
 func (e *{{title .Alias}}) Stream(ctx context.Context, req *pb.StreamRequest, stream pb.{{title .Alias}}_StreamStream) error {
 	log.Infof("Received {{title .Alias}}.Stream request: %v", req)
 	for i := 0; i < int(req.Count); i++ {
+		log.Infof("Sending %d", i)
 		if err := stream.Send(&pb.StreamResponse{
 			Count: int64(i),
 		}); err != nil {
 			return err
 		}
+		time.Sleep(time.Millisecond * 250)
 	}
 	return nil
 }
